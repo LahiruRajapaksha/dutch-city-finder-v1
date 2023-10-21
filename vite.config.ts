259517d9fc7a60cd3cc0a250f8ca719b74fc38d0
lahirurajapaksha <ltwrajapaksha@gmail.com>
2023-10-21 12:28:25 +0200

    update vite proxy and add mui libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+      server: {
+        proxy: {
+            '/static': {
+            target: 'https://simplemaps.com',
+            changeOrigin: true,
+            }
+    },
+    },
   plugins: [react()],
 })
