520527d67f179afaaba50c74b0a093ca0612e09b
lahirurajapaksha <ltwrajapaksha@gmail.com>
2023-10-25 13:06:04 +0200

    code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,34 @@
-import CityCard from "./components/Card/Card";
 import { Box } from "@mui/material";
-import { useFetchDutchCities } from "./utils";
 import Typography from "@mui/material/Typography";
 import SearchBar from "./components/Search/Search";
-import { useState, useEffect } from "react";
-import { CityData } from "./Types/utils-types";
+import { Suspense, lazy, useState } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
-  const { cities, isLoading } = useFetchDutchCities();
-  const [searchCities, setSearchCities] = useState<CityData[]>(cities);
-
-  useEffect(() => {
-    setSearchCities(cities);
-  }, [cities]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const CityCard = lazy(() => import("./components/Card/Card"));
 
   const handleSearchCities = (cityName: string) => {
-    setSearchCities(
-      cities?.filter((cityData) =>
-        cityData.city.toLowerCase().includes(cityName.toLowerCase())
-      )
-    );
+    setSearchTerm(cityName);
   };
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        boxSizing: "border-box",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            mt: 4,
-            mb: 4,
-          }}
-        >
+    <Box display="flex" flexDirection="column" boxSizing="border-box">
+      <Box display="flex" flexDirection="column">
+        <Box display="flex" justifyContent="center" mt={4} mb={4}>
           <Typography variant="h2">Dutch Cities</Typography>
         </Box>
         <SearchBar handleSearchCities={handleSearchCities} />
       </Box>
       <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexWrap: "wrap",
-          // overflow: "auto",
-          flexGrow: 1,
-        }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        flexGrow={1}
       >
-        {isLoading && (
-          <Box mt={5}>
-            <CircularProgress />
-          </Box>
-        )}
-        {!isLoading &&
-          searchCities?.map((city) => (
-            <CityCard
-              key={city.city}
-              city={city.city}
-              province={city.province}
-              population={city.population}
-            />
-          ))}
+        <Suspense fallback={<CircularProgress />}>
+          <CityCard searchTerm={searchTerm} />
+        </Suspense>
       </Box>
     </Box>
   );
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,33 +1,68 @@
 import { Card, CardContent, CardProps, Typography } from "@mui/material";
 import { CityData } from "../../Types/utils-types";
 import { styled } from "@mui/material/styles";
-
-interface CityCardProps extends CityData {}
+import { useEffect, useRef } from "react";
+import { useFetchDutchCities } from "../../utils";
+import "./Card.css";
+interface CityCardProps {
+  searchTerm: string;
+}
 
 const CityCard = (props: CityCardProps) => {
-  const { city, province, population } = props;
+  const { searchTerm } = props;
+  const { cities } = useFetchDutchCities();
+  const cardRef = useRef<HTMLDivElement[]>([]);
 
   const CityCard = styled(Card)<CardProps>(() => ({
-    height: "100",
     width: "20%",
     margin: 10,
+    opacity: 0,
   }));
 
   const CityCardContent = styled(CardContent)<CardProps>(() => ({
     height: 50,
-    width: "100%",
     margin: 5,
   }));
 
-  return (
-    <CityCard>
-      <CityCardContent>
-        <Typography variant="h6">{city}</Typography>
-        <Typography>{`Province:${province}`}</Typography>
-        <Typography>{`Population:${population}`}</Typography>
-      </CityCardContent>
-    </CityCard>
-  );
+  useEffect(() => {
+    const cardObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("card-show");
+            cardObserver.unobserve(entry.target);
+          } else {
+            entry.target.classList.remove("card-show");
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+    if (cardRef.current)
+      cardRef.current.forEach((ref) => cardObserver.observe(ref));
+  }, [cardRef, cities]);
+
+  return cities
+    ?.filter((cityData) =>
+      cityData.city.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .map((city: CityData, index: number) => (
+      <CityCard
+        ref={(e) => (cardRef.current[index] = e!)}
+        key={city.city}
+        className={`card ${
+          city.city.toLowerCase().includes(searchTerm.toLowerCase())
+            ? "card-show"
+            : ""
+        }`}
+      >
+        <CityCardContent>
+          <Typography variant="h6">{city.city}</Typography>
+          <Typography>{`Province: ${city.province}`}</Typography>
+          <Typography>{`Population: ${city.population}`}</Typography>
+        </CityCardContent>
+      </CityCard>
+    ));
 };
 
 export default CityCard;
