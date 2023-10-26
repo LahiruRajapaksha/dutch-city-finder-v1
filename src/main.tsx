9c447b907e5928bd787342a92b479bd22758f26f
lahirurajapaksha <ltwrajapaksha@gmail.com>
2023-10-26 03:08:43 +0200

    add mui style engine provide

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App.tsx";
 import "./index.css";
-
+import { StyledEngineProvider } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <StyledEngineProvider injectFirst>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
