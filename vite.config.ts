2957b5921414e7d83aaebc56e5ddca35de0b0ab2
lahirurajapaksha <ltwrajapaksha@gmail.com>
2023-10-30 23:36:53 +0100

    update proxy for prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: "/dutch-city-finder-v1/",
-      server: {
-        proxy: {
-            '/static': {
-            target: 'https://simplemaps.com',
-            changeOrigin: true,
-            }
+  server: {
+    proxy: {
+      '/static': {
+        target: 'https://simplemaps.com',
+        changeOrigin: true,
+      }
     },
-    },
+  },
   plugins: [react()],
 })
